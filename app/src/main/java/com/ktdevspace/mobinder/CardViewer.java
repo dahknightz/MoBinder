025eb9982e84300121d3ee332527b1d568a47a95
Kyle Thillmann <k.thillmann507@mybvc.ca>
2022-02-12 16:52:15 -0700

    added button animations and styles as well as reworked button functionality CardViewer

--- app/src/main/java/com/ktdevspace/mobinder/CardViewer.java
+++ app/src/main/java/com/ktdevspace/mobinder/CardViewer.java
@@ -2,10 +2,16 @@ package com.ktdevspace.mobinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,10 +20,13 @@ import com.squareup.picasso.Picasso;
 
 public class CardViewer extends AppCompatActivity {
 
-    TextView cardNameTextView;
-    TextView cardIDTextView;
-    TextView nameSearchData;
-    ImageView cardImageView;
+    public TextView cardNameTextView, cardIDTextView, nameSearchData;
+    public ImageView cardImageView;
+    public Button backButton,findButton, addCollectionButton, buyButton;
+    Animation increaseScale, decreaseScale;
+
+    private Handler myHandler = new Handler();
+
 
     SearchAPIService searchAPIService = new SearchAPIService(CardViewer.this);
     RandomAPIService randomAPIService = new RandomAPIService(CardViewer.this);
@@ -33,8 +42,44 @@ public class CardViewer extends AppCompatActivity {
         cardIDTextView = (TextView) findViewById(R.id.cardIDTextView);
         nameSearchData = (TextView) findViewById(R.id.searchTextView);
         cardImageView = (ImageView) findViewById(R.id.cardImageView);
+        backButton = (Button) findViewById(R.id.viewCardBackBtn);
+        findButton = (Button) findViewById(R.id.searchBtn);
+
+        increaseScale = AnimationUtils.loadAnimation(this,R.anim.increase_scale);
+        decreaseScale = AnimationUtils.loadAnimation(this,R.anim.decrease_scale);
 
         randomCard();
+
+        //Back button
+        backButton.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction()== MotionEvent.ACTION_DOWN){
+                    backButton.startAnimation(increaseScale);
+                }else if(motionEvent.getAction()== MotionEvent.ACTION_UP){
+                    backButton.startAnimation(decreaseScale);
+                }
+                backButton.setEnabled(false);
+                myHandler.postDelayed(backBtnDelay, 100);
+                return true;
+            }
+        });
+
+        //Find Button
+        findButton.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction()== MotionEvent.ACTION_DOWN){
+                    findButton.startAnimation(increaseScale);
+                }else if(motionEvent.getAction()== MotionEvent.ACTION_UP){
+                    findButton.startAnimation(decreaseScale);
+                }
+                myHandler.postDelayed(findBtnDelay, 100);
+                return true;
+            }
+        });
 
     }
     public void randomCard() {
@@ -84,51 +129,109 @@ public class CardViewer extends AppCompatActivity {
         });
     }
 
-    //Search Btn
-    public void searchCard(View view) {
-        //Get card name and add to textview
-        searchAPIService.getCardName(nameSearchData.getText().toString(), new SearchAPIService.VolleyResponseListener() {
-            @Override
-            public void onError(String message) {
+    //Back Btn delay runnable
+    private Runnable backBtnDelay = new Runnable() {
+        public void run() {
+            Intent mainPageIntent = new Intent(CardViewer.this, MainActivity.class);
+            startActivity(mainPageIntent);
+        }
+    };
 
-                Toast.makeText(CardViewer.this, "Something broke!", Toast.LENGTH_SHORT).show();
-            }
+    //Find btn delay runnable
+    private Runnable findBtnDelay = new Runnable() {
+        public void run() {
+            //Get card name and add to textview
+            searchAPIService.getCardName(nameSearchData.getText().toString(), new SearchAPIService.VolleyResponseListener() {
+                @Override
+                public void onError(String message) {
 
-            @Override
-            public void onResponse(String cardName) {
+                    Toast.makeText(CardViewer.this, "Something broke!", Toast.LENGTH_SHORT).show();
+                }
 
-                cardNameTextView.setText("Card Name: " + cardName);
-            }
-        });
+                @Override
+                public void onResponse(String cardName) {
 
-        //Get card id and add to text view
-        searchAPIService.getCardID(nameSearchData.getText().toString(), new SearchAPIService.VolleyResponseListener() {
-            @Override
-            public void onError(String message) {
+                    cardNameTextView.setText("Card Name: " + cardName);
+                }
+            });
 
-                Toast.makeText(CardViewer.this, "Something broke!", Toast.LENGTH_SHORT).show();
-            }
+            //Get card id and add to text view
+            searchAPIService.getCardID(nameSearchData.getText().toString(), new SearchAPIService.VolleyResponseListener() {
+                @Override
+                public void onError(String message) {
 
-            @Override
-            public void onResponse(String cardID) {
+                    Toast.makeText(CardViewer.this, "Something broke!", Toast.LENGTH_SHORT).show();
+                }
 
-                cardIDTextView.setText("Multiverse ID: " + cardID);
-            }
-        });
+                @Override
+                public void onResponse(String cardID) {
 
-        //Get card image url and add to imageview
-        searchAPIService.getCardImage(nameSearchData.getText().toString(), new SearchAPIService.VolleyResponseListener() {
-            @Override
-            public void onError(String message) {
+                    cardIDTextView.setText("Multiverse ID: " + cardID);
+                }
+            });
 
-                Toast.makeText(CardViewer.this, "Something broke!", Toast.LENGTH_SHORT).show();
-            }
+            //Get card image url and add to imageview
+            searchAPIService.getCardImage(nameSearchData.getText().toString(), new SearchAPIService.VolleyResponseListener() {
+                @Override
+                public void onError(String message) {
 
-            @Override
-            public void onResponse(String cardImgURL) {
+                    Toast.makeText(CardViewer.this, "Something broke!", Toast.LENGTH_SHORT).show();
+                }
 
-                Picasso.get().load(cardImgURL).into(cardImageView);
-            }
-        });
-    }
+                @Override
+                public void onResponse(String cardImgURL) {
+
+                    Picasso.get().load(cardImgURL).into(cardImageView);
+                }
+            });
+        }
+    };
+
+//    //Search Btn
+//    public void searchCard(View view) {
+//        //Get card name and add to textview
+//        searchAPIService.getCardName(nameSearchData.getText().toString(), new SearchAPIService.VolleyResponseListener() {
+//            @Override
+//            public void onError(String message) {
+//
+//                Toast.makeText(CardViewer.this, "Something broke!", Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onResponse(String cardName) {
+//
+//                cardNameTextView.setText("Card Name: " + cardName);
+//            }
+//        });
+//
+//        //Get card id and add to text view
+//        searchAPIService.getCardID(nameSearchData.getText().toString(), new SearchAPIService.VolleyResponseListener() {
+//            @Override
+//            public void onError(String message) {
+//
+//                Toast.makeText(CardViewer.this, "Something broke!", Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onResponse(String cardID) {
+//
+//                cardIDTextView.setText("Multiverse ID: " + cardID);
+//            }
+//        });
+//
+//        //Get card image url and add to imageview
+//        searchAPIService.getCardImage(nameSearchData.getText().toString(), new SearchAPIService.VolleyResponseListener() {
+//            @Override
+//            public void onError(String message) {
+//
+//                Toast.makeText(CardViewer.this, "Something broke!", Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onResponse(String cardImgURL) {
+//
+//                Picasso.get().load(cardImgURL).into(cardImageView);
+//            }
+//        });
+//    }
 }
